d97de37d3e1093283c230d053a586fc954ae38c5
Ayoub El Ouardi <ayoubelouardioff@gmail.com>
2024-10-20 16:49:53 +0100

    First Commmit yeeh

---
+++ linked_list/main.h
@@ -0,0 +1,20 @@
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+
+/* Singly Linked Lists */
+
+/**
+ * struct slinked_l - A type for linked list data structure.
+ * @data: the data.
+ * @ptr: pointer to the next node in the list.
+ * Description:
+ * This is an implementation for the 
+ */
+typedef struct slinked_l
+{
+	int data;
+	struct slinked_l *ptr;
+}
+
+
+#endif /* LINKED_LIST_H */
